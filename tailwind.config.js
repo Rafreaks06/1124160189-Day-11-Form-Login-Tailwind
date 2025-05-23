374b34935a474f4833c9683f53435cd56dd44a85
Rafreaks06 <mr.rafreaks@gmail.com>
2025-05-23 17:47:38 +0700

    Membuat Form Login dengan fitur darkmode

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
